18e3c7e74f36c3146d998b1c1f4a18ef4812023c
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-10-04 21:59:25 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Vanlife</h1>
-    </div>
+    <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function About() {
+  return <h1>About</h1>
+}
+
+
 
 export default App;
