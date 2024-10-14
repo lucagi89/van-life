5f52c3743180fb5b388a5bc4e2240e8421e801ac
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-10-14 20:30:58 +0100

    adding a filter

--- src/pages/Vans.js
+++ src/pages/Vans.js
@@ -1,6 +1,6 @@
 import VanCard from './VanCard'
 import { useEffect, useState } from 'react'
-import { useSearchParams, Link } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 export default function Vans() {
   const [vans, setVans] = useState([])
@@ -22,10 +22,22 @@ export default function Vans() {
     <div className='content-vans'>
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <Link to='?type=simple' className='van-type simple'>Simple</Link>
-        <Link to='?type=luxury' className='van-type luxury'>Luxury</Link>
-        <Link to='?type=rugged' className='van-type rugged'>Rugged</Link>
-        <Link to='.' className='van-type clear-filters'>Clear filters</Link>
+        <button
+          onClick={() => setSearchParams({type: 'simple'})}
+          className='van-type simple'>Simple
+        </button>
+        <button
+          onClick={() => setSearchParams({type: 'luxury'})}
+          className='van-type luxury'>Luxury
+        </button>
+        <button
+          onClick={() => setSearchParams({type: 'rugged'})}
+          className='van-type rugged'>Rugged
+        </button>
+        <button
+          onClick={() => setSearchParams({})}
+          className='van-type clear-filters'>Clear filters
+        </button>
 
       </div>
       <div className='vans-list'>
