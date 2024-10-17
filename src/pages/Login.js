49120fa7d598597b5e121758a1f1ea571a00cac9
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-10-17 22:07:20 +0100

    implementing a fake login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,48 @@
 import { useState } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+    const [ status, setStatus ] = useState("idle")
+    const [error, setError] = useState(null)
 
     const location = useLocation()
     const message = location.state?.message || ""
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        async function handleUserData() {
+            setStatus("submitting")
+            try {
+                const user = await loginUser(loginFormData)
+                console.log(user)
+            } catch (error) {
+                console.error(error)
+                setError("error")
+            }finally {
+                setStatus("idle")
+        }
+
     }
+    handleUserData()
+    }
+
+    // useEffect(function() {
+    //   async function fetchVans() {
+    //     setLoading(true)
+    //     try {
+    //       const vans = await getVans()
+    //       setVans(vans)
+    //     } catch (error) {
+    //       setError(error)
+    //     }finally {
+    //       setLoading(false)
+    //     }
+    //   }
+    //   fetchVans()
+
+    // }, [])
 
     function handleChange(e) {
         const { name, value } = e.target
@@ -25,6 +57,7 @@ export default function Login() {
         <div className="login-container">
             {message && <h3 className="warning">{message}</h3>}
             <h1>Sign in to your account</h1>
+            {error?.message && <h3 className="warning">{error.message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -40,7 +73,10 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button
+                  disabled={status === "submitting"}>
+                  {status === "submitting" ? "Loading..." : "Login"}
+                </button>
             </form>
         </div>
     )
